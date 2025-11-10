4f831796f340ca0d1737df22c82615f60e87a723
manish7289 <manish7289@github.com>
2025-11-10 13:36:04 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HeadProvider } from "react-head";
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <HeadProvider>
+    <Analytics />
     <App />
   </HeadProvider>
 )
